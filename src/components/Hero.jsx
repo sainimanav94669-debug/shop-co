5febb2d3662e0b41c7e3e7c438362d35280b8c77
Manav-Saini-saini <sainimadan968554@gmail.com>
2026-09-22 11:17:39 +0530

    change hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,13 @@ const Hero = () => {
   return (
     <div>
       This is Hero Section. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis veritatis laborum quam nesciunt amet id consequatur impedit ipsum fugit debitis deserunt sapiente quo ducimus explicabo, reiciendis vero, quasi dicta odit, vel iure repellendus rerum. Necessitatibus ea quos soluta veritatis eveniet, mollitia a deserunt odio ex temporibus iste dolor est nemo possimus harum reiciendis neque iusto maiores fugiat repellendus minima aut quia numquam labore? Totam tempora veritatis aperiam dicta rerum in ullam atque labore magnam? Unde odit voluptatem quidem illo perspiciatis. Nemo cum incidunt sed, earum ex suscipit impedit odio nam adipisci, et quidem delectus cupiditate. Ipsam et saepe enim dignissimos?
+
+      <div>
+            <p>
+                THis is my pargapraph
+            </p>
+            <h2>Thiis is my h2 tag</h2>
+      </div>
     </div>
   )
 }
